42b4560bab500d9bf315e8f8f9af1351a86dcf1a
Sivasankar4 <sankarshivasankar4@gmail.com>
2024-04-06 21:08:16 +0530

    1st push

---
+++ alzheimer/predict.py
@@ -0,0 +1,95 @@
+import tensorflow as tf
+from keras_preprocessing.image import ImageDataGenerator
+from keras_preprocessing import image
+
+import numpy as np
+import easygui
+from keras.models import load_model
+import os
+import tkinter as tk
+from tkinter import *
+from tkinter import filedialog
+from tkinter.filedialog import askopenfile
+from PIL import Image, ImageTk
+my_w = tk.Tk()
+# width = my_w.winfo_width()
+# height = my_w.winfo_height()
+# print(width,height)
+# my_w.geometry("%dx%d"%(width,height))  
+my_w.state("zoomed")
+my_w.wm_title("Alzheimer Disease Prediction")
+my_font1=(18)
+
+bg = ImageTk.PhotoImage(file='bg.png')
+bgLabel = Label(my_w, image=bg)
+bgLabel.place(x=0, y=0 ,relwidth=1,relheight=1)
+
+
+
+# l1 = tk.Label(my_w,text='Upload Files & get results',width=30,font=my_font1,bg='#124178',
+#                    fg='white',)  
+# l1.place(x=550 , y=190,relwidth=0.2,relheight=0.1)
+b1 = tk.Button(my_w, text='Upload Images', 
+   width=20,command = lambda:result(), activebackground='#000080',font=my_font1, bg='#124178',fg='white')
+b1.place(relx=0.4,rely=0.4, relwidth=0.21,relheight=0.1)
+
+print(tf.__version__)
+
+def close():
+   my_w.destroy()
+
+
+titleLabel = Label(my_w, text=' ALIZHEIMER DISEASE PREDICTION', font=('italic', 22, 'bold '), bg='#0C516A',
+                   fg='white')
+titleLabel.place(x=0, y=0,relwidth=1,relheight=0.1)
+
+endbtn=Button(my_w,text="Exit",font='italic 14 bold',bg='#0C516A',fg='white',command=close)
+endbtn.place(relx=0.46,rely=0.8,relwidth=0.1,relheight=0.05)
+
+
+classifierLoad = tf.keras.models.load_model('model2.h5')
+
+
+def result():
+    
+   filename =upload_file()
+   test_image2 = image.load_img(filename, target_size = (200,200))
+   test_image2 = image.img_to_array(test_image2)
+   test_image2 = np.expand_dims(test_image2, axis = 0)   
+   # cnn prediction on the test image
+   result = classifierLoad.predict(test_image2)
+   print(result)
+   b1.place_forget()
+   if result[0][1] == 1:
+       prediction2="NonDemented"
+   elif result[0][0] == 1:
+       prediction2="MildDemented"
+   elif result[0][2] == 1:
+       prediction2="VeryMildDementedd"
+   elif result[0][3] == 1:
+       prediction2="ModerateDemented"
+   
+      
+   #print(prediction2)
+   prediction=prediction2
+   l2 = tk.Label(my_w,text="Result :  "+prediction,width=50,font=my_font1,bg='#AC9005',
+                   fg='white',)  
+   l2.place(x=560, y=550, width=400)
+   return filename 
+       
+def upload_file():   
+    filename =  easygui.fileopenbox()
+    img=Image.open(filename) # read the image file
+    img=img.resize((200,140)) # new width & height
+    img=ImageTk.PhotoImage(img)
+    e1 =tk.Label(my_w)
+    e1.place(relx=0.43, rely=0.4, relwidth=0.15, relheight=0.2)
+    e1.image = img
+    e1['image']=img
+    return filename
+
+my_w.mainloop()
+
+
+
+
